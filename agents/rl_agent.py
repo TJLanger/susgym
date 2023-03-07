763571f314a960eac7e1498b52781d188e8fa307
Trenton Langer <trentonlanger@u.boisestate.edu>
2023-03-06 21:20:41 -0700

    NFQ Training Fix, Add RL Guess Agent

--- agents/rl_agent.py
+++ agents/rl_agent.py
@@ -98,6 +98,101 @@ def randValidGuess(dstate, act):
 
 """
 description:
+-> RL based agent, stochastic actions with RL guided identity guesses
+"""
+class rlGuessSusAgent(agent_helpers.susAgent):
+    def __init__(self, lr=0.001, batch_size=64):
+        # Parent Setup
+        super().__init__()
+        # Basic Setup
+        self.curr_state, self.curr_action = None, None
+        self.num_players = None
+        self.act_space = None
+        # Model Parameters
+        self.lr = lr # Learning rate (for network training)
+        # Models
+        self.batch_size = batch_size
+        self.replay_memory = agent_helpers.ReplayBuffer(batch_size=self.batch_size) # list of [state, action, reward, new state, terminal]
+        self.optimizer = tf.keras.optimizers.Adam(self.lr)
+        self.qnet = None # created on first action select (need act/obs spaces)
+
+    def update(self, next_state, reward, done, info):
+        # Parent
+        super().update(next_state, reward, done, info)
+        # Guessing Updates
+        if done:
+            # Decode State
+            dstate = agent_helpers.decode_state(self.curr_state, self.num_characters)
+            opp_knowledge = np.array(dstate["knowledge"]).flatten()
+            # Network Updates
+            self.replay_memory.store((opp_knowledge, np.array(info["character_ids"]), reward, np.array([])))
+            # Generate Batch
+            if len(self.replay_memory) > self.batch_size:
+                batch_kb, batch_ids, batch_rewards, batch_other = self.replay_memory.sample()
+                batch_kb = tf.convert_to_tensor(batch_kb)
+                batch_ids = tf.convert_to_tensor(batch_ids)
+                self.qnet.fit(batch_kb, batch_ids, verbose=0)
+
+    def close(self):
+        if False:
+            self.qnet.save_weights('./checkpoints/rlGuess_qnet')
+        else:
+            pass
+
+    def pick_action(self, state, act_space, obs_space):
+        # Setup
+        action = np.zeros(act_space.shape, dtype=np.int8)
+        # Network Setup
+        if self.act_space is None:
+            self.act_space = act_space
+        if self.qnet is None:
+            dstate = agent_helpers.decode_state(state, self.num_characters)
+            self._create_models(dstate["knowledge"])
+        # Action Creation
+        if np.any(state[1:4] == 0): # Update to check non-decoded state (RL agent deosnt decode unless needed)
+            # Deocde State
+            dstate = agent_helpers.decode_state(state, self.num_characters)
+            # Character Identity Guesses
+            self._act_charGuess(dstate, action, act_space, obs_space)
+        else:
+            # Deocde State
+            dstate = agent_helpers.decode_state(state, self.num_characters)
+            # Character Die Moves
+            self._act_dieMove(dstate, action, act_space, obs_space)
+            # Action Card Selection
+            self._act_actCards(dstate, action, act_space, obs_space)
+        # Return
+        self.curr_state, self.curr_action = state, action # Track for replay buffer saves in update method
+        return action
+
+    def _act_charGuess(self, decoded_state, action, act_space, obs_space):
+        opp_knowledge = np.array(decoded_state["knowledge"]).flatten()
+        pred_ids = self.qnet.predict(np.array([opp_knowledge]), verbose=0)
+        action[0-len(pred_ids[0]):] = np.rint(pred_ids[0])
+
+    def _create_models(self, dstate_knowledge):
+        # Setup
+        hidden_layers = 7
+        input_size = len(dstate_knowledge)*len(dstate_knowledge[0])
+        self.num_players = len(dstate_knowledge)+1
+        # Create Models
+        hidden_step = 0 #int(input_size/hidden_layers)
+        input = layers.Input(shape=(input_size))
+        prev_layer = input
+        for i in range(0, hidden_layers):
+            outputs = layers.Dense(input_size - i*hidden_step, activation="relu")(prev_layer)
+            prev_layer = outputs
+        outputs = layers.Dense(len(dstate_knowledge), activation="sigmoid")(outputs)
+        outputs = 9*outputs # scale to 0->9 (possible character ids)
+        self.qnet = tf.keras.Model(input, outputs)
+        self.qnet.compile(optimizer=self.optimizer, loss='mse')
+        # print("rlGuess Model:")
+        # print(self.qnet.summary())
+        # Load Weights
+        # self.qnet.load_weights('./checkpoints/rlGuess_qnet')
+
+"""
+description:
 -> RL based agent, epsilon-greedy choice between Q network or stochastic (valid) actions
 """
 class nfqSusAgent(agent_helpers.susAgent):
@@ -108,27 +203,33 @@ class nfqSusAgent(agent_helpers.susAgent):
         self.curr_state, self.curr_action = None, None
         self.num_players = None
         self.act_space = None
+        self.update_cnt = 0
         # Model Parameters
         self.eps = eps # Explore/Exploit rate (epsilon)
         self.lr = lr # Learning rate (for network training)
         self.gamma = gamma # Discount rate (gamma) for future reward scaling
         self.tau = tau # Target update rate (tau) for policy to target network updates
+        self.update_freq = 5 # Only train model every N updates
         # Models
         self.batch_size = batch_size
         self.replay_memory = agent_helpers.ReplayBuffer(batch_size=self.batch_size) # list of [state, action, reward, new state, terminal]
         self.optimizer = tf.keras.optimizers.Adam(self.lr)
         self.nfq, self.target_nfq = None, None # created on first action select (need act/obs spaces)
         # Functionality Controls
-        self.train = False # True => e-greedy, update network; False => 'optimal' actions only
+        self.train = True # True => e-greedy, update network; False => 'optimal' actions only
         # Debug Timing
-        self.times = {"train":0.,"act_roll":0.,"buffer":0.,"tensor":0.,"":0.,"":0.,"":0.,}
+        self.times = {"train":0.,"act_roll":0.,"buffer":0.,"tensor":0.,"update_act_roll":0.,"":0.,"":0.,}
+        self.num_valid_acts = []
 
     def update(self, next_state, reward, done, info):
         # Parent
         super().update(next_state, reward, done, info)
+        # Network Updates
+        self.replay_memory.store((self.curr_state, self.curr_action, reward, next_state))
+        self.update_cnt += 1
         # Generate Batch
         t1 = time.time()
-        if self.train and len(self.replay_memory) > self.batch_size:
+        if self.train and self.update_cnt % self.update_freq == 0 and len(self.replay_memory) > self.batch_size:
             t2 = time.time()
             batch_states, batch_actions, batch_rewards, batch_next_states = self.replay_memory.sample()
             self.times["buffer"] += time.time() - t2
@@ -138,7 +239,10 @@ class nfqSusAgent(agent_helpers.susAgent):
             batch_rewards = tf.convert_to_tensor(batch_rewards)
             batch_next_states = tf.convert_to_tensor(batch_next_states)
             self.times["tensor"] += time.time() - t3
-            batch_next_actions = tf.convert_to_tesnor(self._nfq_action(state) for state in batch_next_states)
+            t4 = time.time()
+            batch_next_actions = [self._nfq_action(state) for state in batch_next_states]
+            self.times["update_act_roll"] += time.time() - t4
+            batch_next_actions = tf.convert_to_tensor(batch_next_actions)
             self._learn(batch_states, batch_actions, batch_rewards, batch_next_states, batch_next_actions)
         self.times["train"] += time.time() - t1
 
@@ -151,8 +255,13 @@ class nfqSusAgent(agent_helpers.susAgent):
 
     def close(self):
         pprint(self.times)
-        if self.train and False:
-            self.nfq.save_weights('./checkpoints/nfq')
+        pprint("Typical Valid Acts:")
+        pprint("    Min: %s" % str(np.min(self.num_valid_acts)))
+        pprint("    Max: %s" % str(np.max(self.num_valid_acts)))
+        pprint("    Med: %s" % str(np.median(self.num_valid_acts)))
+        pprint("    Avg: %s" % str(np.average(self.num_valid_acts)))
+        if self.train and True:
+            self.nfq.save_weights('./checkpoints/nfq5')
         else:
             pass
 
@@ -195,6 +304,7 @@ class nfqSusAgent(agent_helpers.susAgent):
     def _nfq_action(self, state):
         t1 = time.time()
         valid_acts = agent_helpers.get_valid_actions(self.act_space, state, self.num_players)
+        self.num_valid_acts.append(len(valid_acts))
         self.times["act_roll"] += time.time() - t1
         states = np.repeat(state[np.newaxis,:], len(valid_acts), axis=0)
         act_values = self.nfq([states, valid_acts], training=False)
@@ -204,7 +314,7 @@ class nfqSusAgent(agent_helpers.susAgent):
     def _learn(self, batch_states, batch_actions, batch_rewards, batch_next_states, batch_next_actions):
         # Update NFQ
         with tf.GradientTape() as tape:
-            next_rewards = self.target_nfq.predict([batch_next_states, batch_next_actions])
+            next_rewards = self.target_nfq([batch_next_states, batch_next_actions])
             next_qs = batch_rewards + self.gamma*next_rewards
             qs = self.nfq([batch_states, batch_actions])
             loss = tf.math.reduce_mean(tf.math.square(next_qs - qs))
@@ -239,7 +349,7 @@ class nfqSusAgent(agent_helpers.susAgent):
         # print("NFQ Model:")
         # print(self.nfq.summary())
         # Load Weights
-        self.nfq.load_weights('./checkpoints/nfq')
+        self.nfq.load_weights('./checkpoints/nfq4')
         # Copy Policy to Target
         self.target_nfq = tf.keras.models.clone_model(self.nfq)
 
